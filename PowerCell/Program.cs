e8c92d8aab780ce5e563d2b19b359d8ffe0fe258
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-04-23 15:55:32 +0100

    Added Program script from Moodle

--- PowerCell/Program.cs
+++ PowerCell/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
+using System.Globalization;
 
 namespace PowerCell
 {
     public class Program
     {
+        // Argumentos:
+        // args[0]: Nome da célula
+        // args[1]: Número de consumos
+        // args[2]: Quantidade de energia a consumir por operação
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            string name = args[0];
+            int n = int.Parse(args[1]);
+            float amount = float.Parse(args[2]);
+
+            // Cria uma Nova Célula com o Nome Fornecido
+            Cell c = new Cell(name);
+
+            // Mostra o Estado Inicial da Célula
+            Console.WriteLine(c);
+
+            // Consome a Célula n Vezes
+            for (int i = 0; i < n; i++) c.Consume(amount);
+
+            // Mostra o Estado Após os Consumos
+            Console.WriteLine(c);
+
+            // Restaura a Célula e Mostra o Estado Final
+            c.Restore();
+            Console.WriteLine(c);
+
+            // Este programa mostra o seguinte no ecrã (exemplo: Apollo 3 60):
+            //
+            // [Apollo] Level 6: 200/200
+            // [Apollo] Level 1: 20/200
+            // [Apollo] Level 6: 200/200
         }
     }
 }
